a2bb88e85e5ba28ba68057ddc9ad8cdc6153d1b1
Irfan <irfankhan01066@gmial.com>
2026-02-16 18:14:33 +0530

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -549,9 +549,9 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+ <div>
+  <Home/>
+ </div>
   );
 };
 
